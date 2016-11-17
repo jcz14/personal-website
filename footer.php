3d5f77c022b82425b7b775c9409cbf89e9bdb032
Jim Zheng <jcz14@pitt.edu>
2016-11-17 15:54:32 -0800

    created separate files for page header and footer

---
+++ footer.php
@@ -0,0 +1,4 @@
+<footer>
+  <hr />
+  Jim Zheng 2016. Source code available on <a href="https://github.com/jcz14/personal-website">GitHub</a>.
+</footer>
---
+++ header.php
@@ -0,0 +1,9 @@
+<header>
+  <nav id="navigation">
+    <a href="index.php">Home</a>
+    <a href="education.php">Education</a>
+    <a href="work.php">Work</a>
+    <a href="contact.php">Contact</a>
+  </nav>
+  <hr />
+</header>
