2115af6e83bb729ec2a3ae40bd21e1aa65f29aab
Jim Zheng <jcz14@pitt.edu>
2016-11-25 12:10:43 -0800

    updated index page to reflect changes to website

--- index.php
+++ index.php
@@ -15,26 +15,11 @@
     </div>
 
     <h1>Hi there!</h1>
-    <p>I'm Jim, a fourth-year bioinformatics major at the <a href="http://pitt.edu/">University of Pittsburgh</a>. Though this website's primary
-    purpose is for a class project, if you take a look around you'll see that it's eventually going to become something that I use to sell myself to potential
-    employers. Currently contained on this website are some facts about me (what you're looking at right now!), my education and work experience, and a
-    realistic-looking but non-working contact form.</p>
+    <p>I'm Jim, a fourth-year bioinformatics major at the <a href="http://pitt.edu/">University of Pittsburgh</a>. This is my personal website, take a look around and
+    enjoy yourself~!</p>
 
     <h1>About Me</h1>
-    <p>Airplanes make thousands of miles seem like nothing. Though I go to school in Pennsylvania, California is the place I call home, where I go to be with
-    my parents and brother, as well as a bunch of friends that I've known since middle school. When I'm not spending time on-campus during regular academic terms, I'm
-    usually at home playing video games. Either that, or I'm watching whatever's good on TV or working on personal projects. If I'm not home I'm probably doing things
-    with friends or shopping. I guess you can say I'm the typical young adult whose just reached the age of 21, where all the age-related restrictions are no longer
-    applicable.</p>
-    <p>My interests are mostly science and technology-related, as you can tell from my chosen field of study. Ever since I played JumpStart Kindergarten on a Packard
-    Bell 386SX in 1999, I've been incredibly interested in how computers work. They say one can create an entire virtual world from scratch exactly the way the want
-    through nothing but letters, numbers, and some clever ideas, and I think that just about captures the essence of computer programming. It was in high school when
-    I found my other interest, which was biology. More specifically, I discovered that human biology alone had more than enough mysteries to keep me wondering about
-    all sorts of things, and I began to read more and more about all aspects of what makes life possible. At the same time, I learned about all the ways things can go
-    wrong with a living organism, which encompasses anything from a bacterial cell to a human being. As I realized that many anomalies were due to genetic variations,
-    I came upon the field of bioinformatics -- using modern computational tools to solve biological challenges previously thought of as unapproachable. Through research,
-    scientists and analysts have made great innovations in the field of bioinformatics, from protein structure prediction to development of highly-specific targeted-use
-    drugs. With more and more information available, the tools to make use of that information will need to grow along with them.</p>
+    <p>I need to put something here...</p>
 
     <h1>My Goals</h1>
     <p>Right now I'm just an undergraduate student, and I know that that will not be the end of my educational experience. I plan to continue with my graduate studies
