cd7eec55fae78594ba03d004172f74c8cc8c6c30
Jim Zheng <jcz14@pitt.edu>
2016-11-25 11:54:38 -0800

    form submission works now

--- contact.php
+++ contact.php
@@ -1,3 +1,48 @@
+<?php
+  // Since JavaScript is used for form validation we can guarantee that the name field not being blank means the form was filled out correctly
+  if ($_POST["name"] != "") {
+    // Connect to MySQL database
+    $server = "localhost";
+    $user = "root";
+    $password = "password";
+    $db = "personal_website";
+    $mysql_connection = new mysqli($server, $user, $password, $db);
+
+    // Display error if the connection fails
+    if ($mysql_connection->connect_error) {
+      die("There was a problem connecting to the database.");
+    }
+
+    // Store form responses in variables
+    $form_name = $_POST["name"];
+    $form_email = $_POST["email"];
+    $form_message = $_POST["message"];
+
+    // Subject is optional, so if the user didn't put one in we set it to "(no subject)"
+    if ($_POST["subject"] == "") {
+      $form_subject = "(no subject)";
+    } else {
+      $form_subject = $_POST["subject"];
+    }
+
+    // Escape single quotes by replacing them with two single quotes
+    $form_name = str_replace("'", "''", $form_name);
+    $form_email = str_replace("'", "''", $form_email);
+    $form_subject = str_replace("'", "''", $form_subject);
+    $form_message = str_replace("'", "''", $form_message);
+
+    // Insert new row into table
+    $mysql_insert = "INSERT INTO messages VALUES ('". $form_name . "','". $form_email . "','". $form_subject . "','". $form_message . "')";
+
+    // Display message on insertion success or failure
+    if ($mysql_connection->query($mysql_insert) === TRUE) {
+      echo "Thanks! Your message has been submitted, and I will get back to you as soon as I can.";
+    } else {
+      echo "MySQL error: " . $mysql_insert . "<br>" . $mysql_connection->error;
+    }
+  }
+?>
+
 <!doctype html>
 <html lang="en">
   <head>
@@ -11,20 +56,20 @@
     <?php include "header.php"; ?>
 
     <h2 id="welcomeMessage">Write me a message!</h2>
-    <h2 id="completionMessage">Thanks! I'll get back to you as soon as possible!</h2>
 
     <form action="" method="POST" onsubmit="return validate(this);" id="form">
       <fieldset id="messageForm">
         <div class="text">
-          <label for="name">Name: </label>
-          <input type="text" name="name" />
+          <input type="text" id="name" name="name" placeholder="Name"/>
         </div>
         <div class="text">
-          <label for="email">Email: </label>
-          <input type="text" name="email" />
+          <input type="email" id="email" name="email" placeholder="E-Mail"/>
+        </div>
+        <div class="text">
+          <input type="text" id="text" name="subject" placeholder="Subject (optional)"/>
         </div>
         <div class="textarea">
-          <textarea id="message" cols="40" rows="20"></textarea>
+          <textarea id="message" name="message" cols="40" rows="10" placeholder="Message"></textarea>
         </div>
       </fieldset>
       <input type="submit" id="submit" value="Send">
