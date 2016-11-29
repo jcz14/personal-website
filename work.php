5b848a2ee8961212a5960c8efa8ca0afd153c607
Jim Zheng <jcz14@pitt.edu>
2016-11-29 10:12:34 -0800

    added current work

--- work.php
+++ work.php
@@ -9,6 +9,12 @@
   <body>
     <?php include "header.php"; ?>
 
+    <h1>Current Work</h1>
+    <p>I'm currently working on <a href="http://drugquery.csb.pitt.edu/disco/">DISCO</a>, a service that provides results which can be used as a benchmark
+    in predicting the feasibility of protein-ligand docking interactions. The front-end uses PHP and the calculations are done with custom-made Python
+    scripts. The goal of this project is to create a tool which will be used by researchers and analysts in their work as an way to evaluate the results of
+    algorithms that predict protein-ligand interactions in addition to directly predicting protein-ligand interactions.</p>
+
     <h1>2015-2016</h1>
     <p>In my third year of university I was offered a paid full-time position as a data analyst for a paper on the application and proposal of improvements
     to the FDA's structured product labeling (SPL) standard, which uses XML to define a standardized information exchange and definition format for labels
