db34f0d8b56f64951f0fc38ccd8304df16b055dc
Jim Zheng <jcz14@pitt.edu>
2016-12-06 14:46:50 -0800

    changed line wrapping

--- work.php
+++ work.php
@@ -11,22 +11,26 @@
     <?php include "header.php"; ?>
 
     <h1>Current Work</h1>
-    <p>I'm currently working on <a href="http://drugquery.csb.pitt.edu/disco/" target="_blank">DISCO</a>, a service that provides results which can be used as a benchmark
-    in predicting the feasibility of protein-ligand docking interactions. The front-end uses PHP and the calculations are done with custom-made Python
-    scripts. The goal of this project is to create a tool which will be used by researchers and analysts in their work as an way to evaluate the results of
-    algorithms that predict protein-ligand interactions in addition to directly predicting protein-ligand interactions.</p>
+    <p>I'm currently working on <a href="http://drugquery.csb.pitt.edu/disco/" target="_blank">DISCO</a>, a service that
+    provides results which can be used as a benchmark in predicting the feasibility of protein-ligand docking
+    interactions. The front-end uses PHP and the calculations are done with custom-made Python scripts. The goal of this
+    project is to create a tool which will be used by researchers and analysts in their work as a benchmark to evaluate
+    the results of algorithms that predict protein-ligand interactions.</p>
 
     <h1>2015-2016</h1>
-    <p>In my third year of university I was offered a paid full-time position as a data analyst for a paper on the application and proposal of improvements
-    to the FDA's structured product labeling (SPL) standard, which uses XML to define a standardized information exchange and definition format for labels
-    included with all drug products sold within the United States. This position gave me the opportunity to experience the process one takes to publish a
-    paper in a widely-circulated scientific journal, from the planning phase to the publication phase and everything in between.</p>
+    <p>In my third year of university I was offered a paid full-time position as a data analyst for a paper on the
+    application and proposal of improvements to the FDA's structured product labeling (SPL) standard, which uses XML to
+    define a standardized information exchange and definition format for labels included with all drug products sold
+    within the United States. This position gave me the opportunity to experience the process one takes to publish a
+    paper in a widely-circulated scientific journal, from the planning phase to the publication phase and everything in
+    between.</p>
 
     <h1>2013-2014</h1>
-    <p>In the summer after I graduated from high school as well as the summer after my first year of university, I worked as a part-time research assistant
-    at the University of Calfornia in Davis. During my time there I received national-grade biological safety training, which allowed me to safely work with
-    live animal tissue and blood samples. I learned the basics of contemporary lab techniques such as ELISA, SDS-PAGE, buffer preparation, and ion-channel
-    protein separation. I also worked on the data acquisition and storage routines of an in-house program developed by the members of the research lab.</p>
+    <p>My first job was a part-time research assistant position at the University of Calfornia in Davis. During my time
+    there I received national-grade biological safety training, which allowed me to safely work with live animal tissue
+    and blood samples. I learned the basics of contemporary lab techniques such as ELISA, SDS-PAGE, buffer preparation,
+    and ion-channel protein separation. I also worked on the data acquisition and storage routines of an in-house
+    program developed by the members of the research lab.</p>
 
     <?php include "footer.php"; ?>
   </body>
