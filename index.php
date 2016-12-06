9a9b42349da8196fe41633fcbc0b9a3741cf4bdb
Jim Zheng <jcz14@pitt.edu>
2016-12-06 14:57:25 -0800

    changed line wrapping

--- index.php
+++ index.php
@@ -15,16 +15,19 @@
     </div>
 
     <h1>Hi there!</h1>
-    <p>I'm Jim, a fourth-year bioinformatics major at the <a href="http://pitt.edu/" target="_blank">University of Pittsburgh</a>. This is my personal website, take a look around and
-    enjoy yourself~!</p>
+    <p>I'm Jim, a fourth-year bioinformatics major at the <a href="http://pitt.edu/" target="_blank">University of Pittsburgh</a>.
+    This is my personal website, take a look around and enjoy yourself~!</p>
 
     <h1>About Me</h1>
     <p>I need to put something here...</p>
 
     <h1>My Goals</h1>
-    <p>Right now I'm just an undergraduate student, and I know that that will not be the end of my educational experience. I plan to continue with my graduate studies
-    immediately after I get my bachelor's degree while working a part-time job. After I earn my master's I will likely continue working, wherever that may be. My ultimate
-    goal is to secure a position at the National Human Genome Research Institute of the <a href="https://www.nih.gov/">National Institutes of Health</a>.</p>
+    <p>Right now I'm just an undergraduate student, though I know that that will not be the end of my educational
+    experience. I plan to continue with graduate studies immediately after I get my bachelor's degree, as well as find a
+    part-time job. That will most likely be working under a professor, though I'm also open to entry-level positions in
+    corporate settings. My dream job is to work for the National Human Genome Research Institute of the
+    <a href="https://www.nih.gov/" target="_blank">National Institutes of Health</a> and contribute to scientific
+    research that improves both the safety and efficacy of healthcare.</p>
 
     <?php include "footer.php"; ?>
   </body>
