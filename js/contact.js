243060e586031f5bc6bf9a39c8cae4991e597b97
Jim Zheng <jcz14@pitt.edu>
2016-12-06 07:33:52 -0800

    clear form on submit

--- js/contact.js
+++ js/contact.js
@@ -1,18 +1,14 @@
 function validate(form) {
-  // keep track of form validity
-  var valid=true;
-  
-  // display alert message and set validity status to false if any field is left blank
   if(form.name.value=="") {
     alert("Please enter your name.");
-    valid=false;
+    valid = false;
   }
   if(form.email.value=="") {
     alert("Please enter your email address.");
-    valid=false;
+    valid = false;
   }
   if(form.message.value=="") {
     alert("Please type your message.");
-    valid=false;
+    valid = false;
   }
 }
