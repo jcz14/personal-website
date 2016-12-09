c324a8d0060cc1f66b64c230207fd62033942af9
Jim Zheng <jcz14@pitt.edu>
2016-12-08 17:44:56 -0800

    finished "about me" section

--- index.php
+++ index.php
@@ -19,7 +19,35 @@
     This is my personal website, take a look around and enjoy yourself~!</p>
 
     <h1>About Me</h1>
-    <p>I need to put something here...</p>
+    <p>The background image is an overhead view of Davis, California, the city I'm from. It's got a population of
+    roughly 65000 people from all over the world, and as a result I'm exposed to all sorts of cultures and traditions
+    from the friends I made up until high school graduation. I came to study in Pittsburgh because, well, I wanted a
+    change of scenery and some freedom to explore. For starters, the weather's <i>really</i> different, and the city is
+    a lot bigger, both in size and population. Of course I also considered where Pitt's bioinformatics major lies in
+    national rankings, and it's surprisingly high. Safe to say, I feel like I definitely made the right choice coming
+    here and experiencing everything the campus and city has to offer.</p>
+
+    <p>My interests include
+      <ul>
+        <li>Programming: I know C++, Java, Python, Perl, and more recently PHP. In my spare time I often write programs
+        for personal use, whether it's to automate a repetitive task or do something completely new in an intuitive way.</li>
+        <li>Reading: I read a lot. Usually it's blogs or forums, other times it's books. Regardless of the medium, I
+        always try to learn something new from what I read, especially if it's something applicable to what I do. My
+        favorite book genres are science fiction and fantasy.</li>
+        <li>Video games: Yeah, sounds pretty stereotypical for a college student, huh? Most of the ones I play are
+        multi-player, mainly because it lets me interact with real people as opposed to preprogrammed characters. Part
+        of what makes multiplayer games fun is the competition or cooperation they entail, and if I'm not doing anything
+        "productive" I'm probably in a game of Overwatch with my friends.</li>
+        <li>Basketball: I'm nowhere near the level of good, but it's fun exercise and I've been watching it ever since I
+        could understand what it was thanks to my parents. They're not exactly NBA fanatics, but they love watching the
+        televised games while taking guesses as to which team's going to win or who's going to make the game-winning
+        shot. I guess you can say I play it for exercise and because I know how it's played.</li>
+        <li>Music: I play the piano. I like music. Is that enough? No? Well, I enjoy all kinds of music, from heavy
+        pumping EDM to quiet classical compositions. Sometimes I compose music, too, whether it's a self-made cover of
+        something that keeps repeating in my mind or a tune that randomly comes into my consciousness and happens to
+        sound like something coherent.</li>
+    </ul>
+    </p>
 
     <h1>My Goals</h1>
     <p>Right now I'm just an undergraduate student, though I know that that will not be the end of my educational
